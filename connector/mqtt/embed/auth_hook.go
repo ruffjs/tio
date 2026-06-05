30a97e8489f64c4ae36de864dedda14cdd75ee9d
yangcun <yangcun@nanchao.org>
2026-06-05 13:58:56 +0800

    fix(wss): Fix the issue of being unable to connect to wss

--- connector/mqtt/embed/auth_hook.go
+++ connector/mqtt/embed/auth_hook.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"crypto/tls"
 	"log/slog"
+	"net"
+	"reflect"
 
 	mqtt "github.com/mochi-mqtt/server/v2"
 	"github.com/mochi-mqtt/server/v2/packets"
@@ -31,18 +33,65 @@ func extractCNFromCert(cl *mqtt.Client) (string, bool) {
 		return "", false
 	}
 
-	if tlsConn, ok := cl.Net.Conn.(*tls.Conn); ok {
-		state := tlsConn.ConnectionState()
-		if len(state.PeerCertificates) > 0 {
-			cert := state.PeerCertificates[0]
-			if cert.Subject.CommonName != "" {
-				slog.Debug("Extracted CN from client certificate", "cn", cert.Subject.CommonName)
-				return cert.Subject.CommonName, true
-			}
-			return "", true
+	tlsConn, ok := unwrapTLSConn(cl.Net.Conn)
+	if !ok {
+		return "", false
+	}
+
+	state := tlsConn.ConnectionState()
+	if len(state.PeerCertificates) > 0 {
+		cert := state.PeerCertificates[0]
+		if cert.Subject.CommonName != "" {
+			slog.Debug("Extracted CN from client certificate", "cn", cert.Subject.CommonName)
+			return cert.Subject.CommonName, true
 		}
+		return "", true
 	}
 	return "", false
+}
+
+func unwrapTLSConn(conn net.Conn) (*tls.Conn, bool) {
+	if conn == nil {
+		return nil, false
+	}
+
+	if tlsConn, ok := conn.(*tls.Conn); ok {
+		return tlsConn, true
+	}
+
+	return unwrapTLSConnValue(reflect.ValueOf(conn))
+}
+
+func unwrapTLSConnValue(v reflect.Value) (*tls.Conn, bool) {
+	if !v.IsValid() {
+		return nil, false
+	}
+
+	for v.Kind() == reflect.Pointer || v.Kind() == reflect.Interface {
+		if v.IsNil() {
+			return nil, false
+		}
+		if tlsConn, ok := v.Interface().(*tls.Conn); ok {
+			return tlsConn, true
+		}
+		v = v.Elem()
+	}
+
+	if !v.IsValid() || v.Kind() != reflect.Struct {
+		return nil, false
+	}
+
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		if !field.IsValid() || !field.CanInterface() {
+			continue
+		}
+		if tlsConn, ok := unwrapTLSConnValue(field); ok {
+			return tlsConn, true
+		}
+	}
+
+	return nil, false
 }
 
 func (a *authHook) OnConnectAuthenticate(cl *mqtt.Client, pk packets.Packet) bool {
